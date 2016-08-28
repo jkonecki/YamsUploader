b1c21c16692c2ecebb63b9106b3d38e0eef8b575
A. Prooks <aprooks@live.ru>
2016-08-28 22:04:29 +0200

    chore: initial
    Signed-off-by: A. Prooks <aprooks@live.ru>

--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Etg.Yams.Storage;
+
 namespace DeployRunner
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var param= args[0];
+
+            Console.WriteLine("Param provided: "+param+ ". Completed");
         }
     }
 }
